c531ef3aa118a8b99d163f5a1edd68244e7b84f3
Wallace Fares <wallace.fares@webedia-group.com>
2019-06-12 13:51:16 -0300

    removed unused code

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -25,34 +25,24 @@ exports.sourceNodes = async ({ actions }) => {
   const { createNode } = actions;
 
   // fetch raw data from the api
-  const res = await axios.get(`https://jsonplaceholder.typicode.com/todos`);
+  const todos = await axios
+    .get(`https://jsonplaceholder.typicode.com/todos`)
+    .then(res => res.data);
 
   // map into these results and create nodes
-  res.data.map((todo, i) => {
-    // Create your node object
-    const userNode = {
-      // Required fields
-      id: `${i}`,
-      parent: `__SOURCE__`,
-      internal: {
-        type: `todos`, // name of the graphQL query --> allRandomUser {}
-        // contentDigest will be added just after
-        // but it is required
-      },
-      todo,
-      children: [],
-    }
-
-    const contentDigest = crypto
-      .createHash(`md5`)
-      .update(JSON.stringify(userNode + i))
-      .digest(`hex`);
-    // add it to userNode
-
-    userNode.internal.contentDigest = contentDigest;
-
-    createNode(userNode);
-  });
+  todos
+    .map((todo, i) => (
+      {
+        id: `${i}`,
+        parent: `__SOURCE__`,
+        internal: {
+          type: `todos`,
+          contentDigest: `${i}`
+        },
+        todo,
+      }
+    ))
+    .map(node => createNode(node))
 
   return;
 }
