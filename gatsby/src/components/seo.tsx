56c5ee8f1c2605c825eb6f6e91cd5bad6c7fe5a9
Wallace Fares <wallace.fares@webedia-group.com>
2019-06-12 10:05:12 -0300

    added typescript

--- gatsby/src/components/seo.tsx
+++ gatsby/src/components/seo.tsx
@@ -5,12 +5,19 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
+import React, { FunctionComponent } from "react"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, title }) {
+
+interface IProps {
+  description?: string,
+  lang?: string,
+  meta: any,
+  title: string,
+}
+
+const SEO: FunctionComponent<IProps> = ({ description, lang, meta, title }) => {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -78,11 +85,5 @@ SEO.defaultProps = {
   description: ``,
 }
 
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
-}
 
 export default SEO
